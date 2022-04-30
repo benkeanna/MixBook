92dd6aed7b01b0b6f2b9dab2a20aaf2b50b673cf
Tomas Storc <tomas.storc@gmail.com>
2022-04-30 17:52:55 +0200

    removed json

--- frontend/src/contexts/filter.context.jsx
+++ frontend/src/contexts/filter.context.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useState } from "react";
 import { RecipesContext } from "./recipes.context";
-import recipesData from "../recipes.json";
 
 const filterIngredients = [];
 
@@ -14,7 +13,7 @@ export const FilterContext = createContext({
 
 export const FilterProvider = ({ children }) => {
   const [showFilter, setShowFilter] = useState(false);
-  const { setRecipes } = useContext(RecipesContext);
+  const { setRecipes, initRecipes } = useContext(RecipesContext);
 
   const addIngredient = (ingredient) => {
     filterIngredients.push(ingredient);
@@ -28,14 +27,13 @@ export const FilterProvider = ({ children }) => {
 
   const filterRecipes = () => {
     if (filterIngredients.length === 0) {
-      setRecipes(recipesData);
+      setRecipes(initRecipes);
     } else {
-      const filteredRecipes = recipesData.filter((recipe) => {
+      const filteredRecipes = initRecipes.filter((recipe) => {
         const recipeIngredients = recipe.ingredients;
-        const isMatch = recipeIngredients.some((ingredient) =>
+        return recipeIngredients.some((ingredient) =>
           filterIngredients.includes(ingredient.toLowerCase())
         );
-        return isMatch;
       });
       setRecipes(filteredRecipes);
     }
