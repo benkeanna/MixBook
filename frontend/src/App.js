3c2acf0199476d9567135544eb059b1e7414cf33
Tomas Storc <tomas.storc@gmail.com>
2022-05-02 14:05:40 +0200

    added route to ingredients

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home";
+import Ingredients from "./pages/ingredients";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/ingredients" element={<Ingredients />} />
       </Routes>
     </>
   );
