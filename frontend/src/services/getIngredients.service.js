b8d97610e004a3b282331dd944e588bcda4b67f7
Tomas Storc <tomas.storc@gmail.com>
2022-05-03 09:49:25 +0200

    filter area changes

--- frontend/src/services/getIngredients.service.js
+++ frontend/src/services/getIngredients.service.js
@@ -5,7 +5,7 @@ import { API_SERVER } from "../utils/server_url.util";
  * @returns {Promise} Promise of the fetch request to the api of the ingredients
  */
 export default function getIngredients() {
-  return fetch(`${API_SERVER}/ingredients`)
+  return fetch(`http://localhost:3001/ingredients`)
     .then((res) => res.json())
     .then((data) => data);
 }
