80f33b928a5dddf6c08799453162168f55b3c733
Tomas Storc <tomas.storc@gmail.com>
2022-05-03 14:46:37 +0200

    added prop types

--- frontend/src/components/button/button.component.jsx
+++ frontend/src/components/button/button.component.jsx
@@ -1,7 +1,13 @@
+import PropTypes from "prop-types";
+
 import "./button.styles.scss";
 
 const Button = ({ type, children, clickHandler }) => {
   return <button className={` button ${type} `}>{children}</button>;
+};
+
+Button.propTypes = {
+  type: PropTypes.oneOf(["add", "remove"]).isRequired,
 };
 
 export default Button;
