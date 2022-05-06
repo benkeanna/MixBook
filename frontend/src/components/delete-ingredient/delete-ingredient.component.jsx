ea3d5bcdaa1866bbc19bb9d37263623916c190cb
Tomas Storc <tomas.storc@gmail.com>
2022-05-06 09:58:38 +0200

    delete ingredient done

--- frontend/src/components/delete-ingredient/delete-ingredient.component.jsx
+++ frontend/src/components/delete-ingredient/delete-ingredient.component.jsx
@@ -4,16 +4,32 @@ import Button from "../button/button.component";
 
 import { DialogsContext } from "../../contexts/dialogs.contexts";
 
+import deleteIngredient from "../../services/deleteIngredient.service";
+
 import "./delete-ingredient.styles.scss";
 
 const DeleteIngredient = () => {
-  const { setShowDeleteIngredientDialog } = useContext(DialogsContext);
+  const { setShowDeleteIngredientDialog, deleteIngredientId } =
+    useContext(DialogsContext);
   return (
     <div className="delete-ingredient-container">
       Are you sure?
       <div className="confirm-container">
         <div>
-          <Button type="delete">yes</Button>
+          <Button
+            onClick={() => {
+              deleteIngredient(deleteIngredientId)
+                .then(() => {
+                  setShowDeleteIngredientDialog(false);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }}
+            type="add"
+          >
+            yes
+          </Button>
         </div>
         <div>
           <Button
